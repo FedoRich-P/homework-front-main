e5bcba33c1760b46a68176d38c6ee42bb791c6a4
FedoRich-P <panchev_fedor@mail.ru>
2024-09-02 10:35:36 +0300

    h.w. 4

--- src/HW/4/Button.tsx
+++ src/HW/4/Button.tsx
@@ -1,11 +1,11 @@
 type ButtonPropsType = {
-  callBack: any // НУЖНО ПРОТИПИЗИРОВАТЬ
-  name: any // НУЖНО ПРОТИПИЗИРОВАТЬ
+  callBack: () =>void;//any // НУЖНО ПРОТИПИЗИРОВАТЬ
+  name: string;//any // НУЖНО ПРОТИПИЗИРОВАТЬ
 };
 
 export const Button = (props: ButtonPropsType) => {
   const callBackHandler = () => {
-    // НУЖНО ДОПИСАТЬ
+    props.callBack()
   };
 
   return (
